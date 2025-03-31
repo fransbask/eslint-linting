fd9366a8ff61b6f10db1f4dd1428448a03854ea8
Frans Bask <frans.bask@edu.turkuamk.fi>
2025-03-31 20:30:02 +0300

    Add price-list module with linting errors

---
+++ price-list.js
@@ -0,0 +1,11 @@
+var aPriceList;
+
+export var setPriceList = (priceList) => {
+    aPriceList = priceList;
+};
+
+export var getPrice = (itemId, quantity) => {
+    return 500;
+};
+
+var price = getPrice(); 
